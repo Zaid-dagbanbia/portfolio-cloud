8d8c6e2418a85965b032521be14117a40f4ce2de
Zaid <dagbanbia@gmail.com>
2026-03-07 12:52:58 +0100

    my first project commit

---
+++ portfolio-cloud/app/layout.js
@@ -0,0 +1,29 @@
+import { Outfit, Ovo } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"]
+});
+
+const ovo = Ovo({
+  subsets: ["latin"],
+  weight: ["400"]
+});
+
+export const metadata = {
+  title: "Portfolio - DMZ",
+  description: "DMZ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme bg-white`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ portfolio-cloud/app/page.js
@@ -0,0 +1,14 @@
+'use client'
+
+import Navbar from "./components/Navbar"
+
+
+
+// pages/index.js or app/page.js
+export default function Home() {
+  return (
+    <>
+    <Navbar />
+    </>
+  )
+}
